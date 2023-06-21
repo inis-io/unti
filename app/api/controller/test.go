44d9a617e8545f9413a436ef03cb20cf78a43554
兔子 <97783391@qq.com>
2023-06-21 19:09:52 +0800

    v1.0.0
    v1.0.0

--- app/api/controller/test.go
+++ app/api/controller/test.go
@@ -7,14 +7,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-pay/gopay"
 	"github.com/go-pay/gopay/alipay"
-	JWT "github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
-	"github.com/spf13/cast"
-	"github.com/unti-io/go-utils/utils"
 	"inis/app/facade"
 	"mime/multipart"
 	"strings"
-	"time"
 )
 
 type Test struct {
@@ -30,7 +26,6 @@ func (this *Test) IGET(ctx *gin.Context) {
 	allow := map[string]any{
 		"request": this.request,
 		"alipay":  this.alipay,
-		"system":  this.system,
 	}
 	err := this.call(allow, method, ctx)
 
@@ -51,7 +46,6 @@ func (this *Test) IPOST(ctx *gin.Context) {
 		"notify-url": this.notifyUrl,
 		"request":    this.request,
 		"upload":     this.upload,
-		"qq":         this.qq,
 	}
 	err := this.call(allow, method, ctx)
 
@@ -108,162 +102,6 @@ func (this *Test) INDEX(ctx *gin.Context) {
 	}
 
 	this.json(ctx, res, facade.Lang(ctx, "好的！"), 200)
-}
-
-type JwtStruct struct {
-	request  JwtRequest
-	response JwtResponse
-}
-
-// JwtRequest - JWT请求
-type JwtRequest struct {
-	// 过期时间
-	Expire  int64        `json:"expire"`
-	// 颁发者签名
-	Issuer  string       `json:"issuer"`
-	// 主题
-	Subject string       `json:"subject"`
-	// 密钥
-	Key     string       `json:"key"`
-}
-
-// JwtResponse - JWT响应
-type JwtResponse struct {
-	Text  string         `json:"text"`
-	Data  map[string]any `json:"data"`
-	Error error          `json:"error"`
-	Valid int64          `json:"valid"`
-}
-
-// Jwt - 入口
-func Jwt(request ...JwtRequest) *JwtStruct {
-
-	if len(request) == 0 {
-		request = append(request, JwtRequest{})
-	}
-
-	// 过期时间
-	if request[0].Expire == 0 {
-		request[0].Expire = cast.ToInt64(utils.Calc(facade.CryptToml.Get("jwt.expire", "7200")))
-	}
-
-	// 颁发者签名
-	if utils.Is.Empty(request[0].Issuer) {
-		request[0].Issuer = cast.ToString(facade.CryptToml.Get("jwt.issuer", "inis.cn"))
-	}
-
-	// 主题
-	if utils.Is.Empty(request[0].Subject) {
-		request[0].Subject = cast.ToString(facade.CryptToml.Get("jwt.subject", "inis"))
-	}
-
-	// 密钥
-	if utils.Is.Empty(request[0].Key) {
-		request[0].Key = cast.ToString(facade.CryptToml.Get("jwt.key", "inis"))
-	}
-
-	return &JwtStruct{
-		request: request[0],
-		response: JwtResponse{
-			Data: make(map[string]any),
-		},
-	}
-}
-
-// Create - 创建JWT
-func (this *JwtStruct) Create(data map[string]any) (result JwtResponse) {
-
-	type JwtClaims struct {
-		Data map[string]any `json:"data"`
-		JWT.RegisteredClaims
-	}
-
-	IssuedAt  := JWT.NewNumericDate(time.Now())
-	ExpiresAt := JWT.NewNumericDate(time.Now().Add(time.Second * time.Duration(this.request.Expire)))
-
-	item, err := JWT.NewWithClaims(JWT.SigningMethodHS256, JwtClaims{
-		Data: data,
-		RegisteredClaims: JWT.RegisteredClaims{
-			IssuedAt:  IssuedAt,				// 签发时间戳
-			ExpiresAt: ExpiresAt,				// 过期时间戳
-			Issuer:    this.request.Issuer,		// 颁发者签名
-			Subject:   this.request.Subject,	// 签名主题
-		},
-	}).SignedString([]byte(this.request.Key))
-
-	if err != nil {
-		this.response.Error = err
-		return this.response
-	}
-
-	this.response.Text = item
-
-	return this.response
-}
-
-// Parse - 解析JWT
-func (this *JwtStruct) Parse(token any) (result JwtResponse) {
-
-	type JwtClaims struct {
-		Data map[string]any `json:"data"`
-		JWT.RegisteredClaims
-	}
-
-	item, err := JWT.ParseWithClaims(cast.ToString(token), &JwtClaims{}, func(token *JWT.Token) (any, error) {
-		return []byte(this.request.Key), nil
-	})
-
-	if err != nil {
-		facade.Log.Error(map[string]any{
-			"error":     err,
-			"func_name": utils.Caller().FuncName,
-			"file_name": utils.Caller().FileName,
-			"file_line": utils.Caller().Line,
-		}, "JWT解析错误")
-		this.response.Error = err
-		return this.response
-	}
-
-	if key, _ := item.Claims.(*JwtClaims); item.Valid {
-		this.response.Data  = key.Data
-		this.response.Valid = key.RegisteredClaims.ExpiresAt.Time.Unix() - time.Now().Unix()
-	}
-
-	return this.response
-}
-
-func (this *Test) qq(ctx *gin.Context) {
-
-	params := this.params(ctx)
-
-	if params["message_type"] == "private" {
-		fmt.Println(utils.Json.Encode(params))
-
-		item := utils.Curl(utils.CurlRequest{
-			Method: "GET",
-			Url:    "http://localhost:5700/send_private_msg",
-			Query: map[string]any{
-				"user_id": cast.ToString(params["user_id"]),
-				"message": cast.ToString(params["message"]),
-			},
-		}).Send()
-
-		if item.Error != nil {
-			fmt.Println("发送失败", item.Error.Error())
-			return
-		}
-
-		fmt.Println("发送成功", item.Json)
-	}
-
-	this.json(ctx, params, facade.Lang(ctx, "好的！"), 200)
-}
-
-func (this *Test) system(ctx *gin.Context) {
-
-	params := this.params(ctx)
-
-	this.json(ctx, params, facade.Lang(ctx, "好的！"), 200)
 }
 
 // INDEX - GET请求本体
